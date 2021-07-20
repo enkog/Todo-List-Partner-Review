6457702fa0e3dbefe0b904218f4f5d27332bda25
enkog <nkirukaogu@gmail.com>
2021-07-20 16:46:52 +0100

    Declare array of simple to do tasks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import _ from "lodash";
 import "./style.css";
+
+const todoArr = [
+  { description: "Go to the mall", completed: false, index: 1 },
+  { description: "Do the laundry", completed: false, index: 2 },
+  { description: "Complete todo list project", completed: false, index: 3 },
+];
