b36620818815422e2e20c4bfcf4a6ab9b3aca588
enkog <nkirukaogu@gmail.com>
2021-07-23 17:24:27 +0100

    Add click events to clear all selected items

--- src/index.js
+++ src/index.js
@@ -9,13 +9,22 @@ const actions = new LocalStorageActions();
 const taskUtils = new TaskUtils(actions);
 const localTodos = actions.get();
 
+// Edit description
 const editTodo = (ctx) => {
   const {
     li, labelMenu, deleteIcon, label,
   } = ctx;
+
+  // hide three dots
   labelMenu.classList.add('hidden');
+
+  // show delete icon
   deleteIcon.classList.remove('hidden');
+
+  // make field editable
   const currDesc = li.textContent;
+
+  // create an input field and prepopulate with current description
   const editInput = document.createElement('input');
   editInput.type = 'text';
   editInput.className = 'edit-todo-input';
@@ -23,20 +32,32 @@ const editTodo = (ctx) => {
   editInput.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
       e.preventDefault();
+      // call edit
       const idx = localTodos.findIndex((todo) => todo.description === currDesc);
+
       taskUtils.editTaskDesc(editInput.value, idx);
+
+      // reload page
       window.location.reload();
     }
   });
+
+  // replace li's textcontent with editinput
   li.removeChild(label);
   li.appendChild(editInput);
 };
 
 const deleteTodo = (li) => {
+  // find input class edit-todo-input in li
   const input = li.querySelector('.edit-todo-input');
   const description = input.value;
+
   const idx = localTodos.findIndex((todo) => todo.description === description);
+
+  // delete task at given index
   taskUtils.deleteTask(idx);
+
+  // reload page
   window.location.reload();
 };
 
@@ -53,18 +74,34 @@ function displayTodo(arr, actions) {
 
     const checkBox = document.createElement('input');
     checkBox.setAttribute('type', 'checkbox');
+    checkBox.className = 'check';
     checkBox.checked = e.completed;
 
     const label = document.createElement('label');
     const labelMenu = document.createElement('i');
     labelMenu.className = 'fas fa-ellipsis-v';
 
+    const tickIcon = document.createElement('i');
+    tickIcon.className = 'fas fa-check hidden';
+
     const deleteIcon = document.createElement('i');
     deleteIcon.className = 'far fa-trash-alt hidden';
 
     const tasks = { li, arr, actions };
+    tickIcon.addEventListener('click', () => {
+      taskComplete(tasks);
+      tickIcon.classList.add('hidden');
+      checkBox.classList.remove('hidden');
+      checkBox.checked = e.completed;
+      label.style.textDecoration = 'none';
+    });
 
-    checkBox.addEventListener('click', taskComplete.bind(null, tasks));
+    checkBox.addEventListener('click', () => {
+      taskComplete(tasks);
+      checkBox.classList.add('hidden');
+      tickIcon.classList.remove('hidden');
+      label.style.textDecoration = 'line-through';
+    });
 
     labelMenu.addEventListener('click', editTodo.bind(null, {
       li, labelMenu, deleteIcon, label,
@@ -74,8 +111,9 @@ function displayTodo(arr, actions) {
 
     label.appendChild(document.createTextNode(e.description));
     li.appendChild(checkBox);
+    li.appendChild(tickIcon);
     li.appendChild(label);
-    li.appendChild(labelMenu);
+    li.appendChild(labelMenu); {}
     li.appendChild(deleteIcon);
     ul.appendChild(li);
   });
@@ -90,8 +128,12 @@ const addOnEnter = document.querySelector('.add-todo-input');
 
 const addTodo = () => {
   const description = addOnEnter.value;
+
   const index = localTodos.length + 1;
+  // create new task object
   const task = new Task(description, index);
+
+  // add new task to storage
   if (description.length > 0) {
     taskUtils.addTask(task, actions);
     window.location.reload();
@@ -107,3 +149,11 @@ function handleEnter(event) {
 
 addBtn.addEventListener('click', addTodo);
 addOnEnter.addEventListener('keyup', handleEnter);
+
+// Clear completed tasks
+const completedTask = document.querySelector('.todo-footer');
+
+completedTask.addEventListener('click', () => {
+  taskUtils.clearCompleted();
+  window.location.reload();
+});
